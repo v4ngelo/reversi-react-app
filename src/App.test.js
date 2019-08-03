91b2216789ad2b7421d648fd4e6d1619e7e27436
Anabella Racioppi <anabella.racioppi@yahoo.com.ar>
2019-08-03 00:12:06 -0300

    Fix broken test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
 import App from './App';
+
+const store = createStore(reducer);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
